897c355643ddad86776af62b4f7e8e46a0a83341
yu10p <eason31003@gmail.com>
2022-11-13 01:00:42 +0800

    v0.2

--- search/Server/searchFunction.js
+++ search/Server/searchFunction.js
@@ -1,13 +1,15 @@
 const sqlite3 = require('sqlite3')
 let db = new sqlite3.Database('../product_db/product.db')
 
-exports.search = function(Name){
+exports.search = function(productName){
     //如果商品名不為空，搜尋特定商品
-    if(Name!=''){
-        console.log('entry If')
+    if(productName!=''){
+        //字串處理，輸入部分關鍵字也可查詢，但無法處理空格
+        productName = '%'+productName+'%'
+        console.log('entry If')   
         //取得特定商品資料
-        db.each("SELECT * FROM product WHERE productName=$Name",{$Name:Name}, (err,row) => {
-            console.log(row.productName+" = "+row.productPrice)
+        db.each("SELECT * FROM product WHERE productName LIKE $productName",{$productName:productName}, (err,row) => {
+            console.log(row.productName+" : "+row.productPrice+"，from : "+row.userId)
         })
     //如果商品名為空，搜尋所有商品
     }else{  
@@ -15,14 +17,7 @@ exports.search = function(Name){
         //取得所有商品資料
         db.each("SELECT * FROM product", (err,row) => {
             console.log(row.productName+" = "+row.productPrice)
+            console.log(row)
         })
     }
-    
 }
-
-
-
-test_module = function() {
-    return 'test';
-}
-
