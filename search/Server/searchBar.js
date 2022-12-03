95f3efb108f87d9edc2522ac852bb133914e3bfb
yu10p <eason31003@gmail.com>
2022-12-03 21:30:47 +0800

    v0.3

--- search/Server/searchBar.js
+++ search/Server/searchBar.js
@@ -13,7 +13,8 @@ app.route('/')
     //當從'/'取得GET Request將serchBar.html回傳給他
     .get((req,res) => {
         res.render('searchBar',{
-            name: 'start'
+            result: '',
+            productName: ''
         })
     })
 
@@ -21,14 +22,28 @@ app.route('/product')
     //當從'/product'取得GET Request將他搜尋的商品資訊顯示在console上
     .get((req,res) => {
         //取得傳入的商品名並搜尋
-        search.search(req.query.productName)
+        received = search.search(req.query.productName)
+
         res.render('searchBar',{
-            name: req.query.productName
+            result: received,
+            productName: req.query.productName
         })
     })
+
+//===施工中===
+app.route('/Myproduct')
+    //當從'/product'取得GET Request將他搜尋的商品資訊顯示在console上
+    .get((req,res) => {
+        //取得傳入的商品名並搜尋
+        received = search.Mysearch(req.query.productName,req.query.userId)
+
+        res.render('searchBar',{
+            result: received,
+            productName: req.query.productName
+        })
+    })
+//===施工中===
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-
--- search/Server/searchFunction.js
+++ search/Server/searchFunction.js
@@ -1,23 +1,46 @@
-const sqlite3 = require('sqlite3')
-let db = new sqlite3.Database('../product_db/product.db')
+const better_sqlite = require('better-sqlite3')
+const db = new better_sqlite('../product_db/product.db')
 
 exports.search = function(productName){
     //如果商品名不為空，搜尋特定商品
     if(productName!=''){
         //字串處理，輸入部分關鍵字也可查詢，但無法處理空格
         productName = '%'+productName+'%'
-        console.log('entry If')   
+        //console.log('entry If')   
+
         //取得特定商品資料
-        db.each("SELECT * FROM product WHERE productName LIKE $productName",{$productName:productName}, (err,row) => {
-            console.log(row.productName+" : "+row.productPrice+"，from : "+row.userId)
-        })
+        var result = db.prepare("SELECT * FROM product WHERE productName like ?").all(productName)
+        //console.log(result)
+
+        
     //如果商品名為空，搜尋所有商品
     }else{  
         console.log('entry Else')
-        //取得所有商品資料
-        db.each("SELECT * FROM product", (err,row) => {
-            console.log(row.productName+" = "+row.productPrice)
-            console.log(row)
-        })
+        var result = db.prepare("SELECT * FROM product").all()
+        //console.log(result)
     }
+    return result
 }
+
+//===施工中===
+exports.Mysearch = function(productName,userId){
+    //如果商品名不為空，搜尋特定商品
+    if(productName!=''){
+        //字串處理，輸入部分關鍵字也可查詢，但無法處理空格
+        productName = '%'+productName+'%'
+        //console.log('entry If')   
+
+        //取得特定商品資料
+        var result = db.prepare("SELECT * FROM product WHERE productName like ? AND userId like ?").all(productName,userId)
+        //console.log(result)
+
+        
+    //如果商品名為空，搜尋所有商品
+    }else{  
+        console.log('entry Else')
+        var result = db.prepare("SELECT * FROM product WHERE userId like ?").all(userId)
+        //console.log(result)
+    }
+    return result
+}
+//===施工中===
